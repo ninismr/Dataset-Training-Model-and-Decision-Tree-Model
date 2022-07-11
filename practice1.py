a25b3ada527cb792f026d35e0f81653291527c09
Mulya Fajar Ningsih Alwi <98800243+ninismr@users.noreply.github.com>
2022-07-11 21:13:47 +0700

    Delete practice1.py

--- practice1.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv("D:\PYTHON PRACTICE\Lab Assignment 14B\gold_karat.csv")
-print(df.head())
-
-plt.scatter(df['karat'], df['Sell Price($)'])
